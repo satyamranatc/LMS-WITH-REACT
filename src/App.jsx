6840ea0052f632c5c52e60aea09189cf1d493a96
Satyam Rana <57563008+satyamranatc@users.noreply.github.com>
2024-06-20 12:35:06 +0530

    Done - day 2

--- src/App.jsx
+++ src/App.jsx
@@ -11,8 +11,13 @@ import NavBar from "./Components/NavBar"
 import Home from "./Pages/Home/Home.jsx"
 import Courses from "./Pages/Courses/Courses.jsx"
 import Practice from "./Pages/Practice/Practice.jsx"
+
 import Quiz from "./Pages/Practice/Quizes/Quiz.jsx"
 import Questions from "./Pages/Practice/Questions/Questions.jsx"
+
+import HtmlCss from './Pages/Practice/Quizes/HtmlCss.jsx'
+import JavaScript from './Pages/Practice/Quizes/JavaScript.jsx'
+import ReactPage from './Pages/Practice/Quizes/ReactPage.jsx'
 
 // !---------------------------------------
 
@@ -29,8 +34,13 @@ export default function App() {
         <Route path="/" element={<Home/>}/>
         <Route path="/Courses" element={<Courses/>}/>
         <Route path="/Practice" element={<Practice/>}/>
-        <Route path="/Practice/Quiz" element={<Quiz/>}/>
+        
         <Route path="/Practice/Questions" element={<Questions/>}/>
+        <Route path="/Practice/Quiz" element={<Quiz/>}/>
+        
+        <Route path="/Practice/Quiz/HtmlCss" element={<HtmlCss/>}/>
+        <Route path="/Practice/Quiz/JavaScript" element={<JavaScript/>}/>
+        <Route path="/Practice/Quiz/React" element={<ReactPage/>}/>
         
         
       </Routes> 
--- src/Pages/Practice/Quizes/Quiz.jsx
+++ src/Pages/Practice/Quizes/Quiz.jsx
@@ -1,12 +1,44 @@
-import React from 'react'
-import Practice from '../Practice'
-export default function Quiz() {
+import React from 'react';
+import './Quiz.css'; // Import the CSS file
+import { Link } from 'react-router-dom';
+const placeholderImageURL = 'https://placehold.co/600x400';
+
+const Quiz = () => {
   return (
     <div>
-       <Practice/>
-        <center>
-            <h1>Quiz Page</h1>
-        </center>
+      <center>
+        <h1>Quiz Page</h1>
+      </center>
+      <div className="quiz-card-container">
+
+        
+        <Link to={"/Practice/Quiz/HtmlCss"}>
+            <div className="quiz-card">
+              <img className="quiz-card-image" src={placeholderImageURL} alt="HTML-CSS" />
+              <h3 className="quiz-card-title">HTML-CSS</h3>
+            </div>
+        </Link>
+
+        <Link to={"/Practice/Quiz/JavaScript"}>
+            <div className="quiz-card">
+              <img className="quiz-card-image" src={placeholderImageURL} alt="JavaScript" />
+              <h3 className="quiz-card-title">JavaScript</h3>
+            </div>
+        </Link>
+
+
+        <Link to={"/Practice/Quiz/React"}>
+            <div className="quiz-card">
+              <img className="quiz-card-image" src={placeholderImageURL} alt="React" />
+              <h3 className="quiz-card-title">React</h3>
+            </div>
+        </Link>
+
+
+
+      </div>
     </div>
-  )
-}
+  );
+};
+
+export default Quiz;
